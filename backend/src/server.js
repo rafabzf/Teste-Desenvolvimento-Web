ab90e268205e0d51542909f0d73a41929a89cbb6
Rafael Bleyer Zanette Furtado <rafinhabzf@gmail.com>
2024-04-23 14:45:32 -0300

    Implementando a função de calculo dos números primos

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,32 @@ app.use(express.json());
 const port = 3000;
 
 app.post('/primos', (req, res) => {
-    //função primos
-
-    res.json({resultado});
+    const { number } = req.body;
+    const primeNumbers = calculatePrimeNumber(parseInt(number));
+    res.json({resultado: primeNumbers.length});
 });
+
+const calculatePrimeNumber = (number) => {
+    const numList = [];
+    for (let index = 2; i < number; index++) {
+        if (isPrimeNumber(index)) {
+            numList.push(index);
+        }
+    }
+    return numList;
+};
+
+const isPrimeNumber = (num) => {
+    if (num <= 1) {
+        return false;
+    }
+    for (let index = 2; index < num; index++) {
+        if (num % index === 0) {
+            return false;
+        }
+    }
+    return true;
+};
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
