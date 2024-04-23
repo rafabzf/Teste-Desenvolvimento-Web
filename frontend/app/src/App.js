126e83b75bf7df8e2bf5bc1c1f750b9b046e1606
Rafael Bleyer Zanette Furtado <rafinhabzf@gmail.com>
2024-04-23 01:33:17 -0300

    Começando a implementar o frontend

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState} from 'react';
 
 function App() {
+  const [num, setNum] = useState('');
+  const [result, setResult] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Calculadora de números primos
+      </h1>
+      <input
+        type='number'
+        value={'...'}
+        onChange={(e) => setNum(e.target.value)}
+        placeholder='Digite um número'
+      ></input>
+      <button onClick={'...'}>Calcular</button>
     </div>
   );
 }
